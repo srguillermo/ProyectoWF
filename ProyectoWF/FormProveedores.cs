1ec914fdabb021427ac5df718c054b3a8774497e
Angel Gonzalez Sala <angel.gonzalez.sala2013@gmail.com>
2017-11-17 00:07:28 +0100

    creada GUI y funcionalidad logo

--- ProyectoWF/FormProveedores.cs
+++ ProyectoWF/FormProveedores.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,24 @@ namespace ProyectoWF {
 	public partial class FormProveedores : Form {
 		public FormProveedores() {
 			InitializeComponent();
+		}
+
+		private void btExaminar_Click(object sender, EventArgs e) {
+			OpenFileDialog od = new OpenFileDialog();
+
+			od.InitialDirectory = ".";
+			od.Filter = "Imagenes de mapa de bits (*)|*.bmp;*.gif;*.jpg;";
+			od.RestoreDirectory = true;
+
+			if (od.ShowDialog() == DialogResult.OK) {
+				try {
+					tbLogo.Text = od.FileName;
+					pbLogo.ImageLocation = tbLogo.Text;
+				} catch (Exception ex) {
+					MessageBox.Show("Error: No se ha podido leer el archivo.\n " + ex.Message);
+				}
+			}
+
 		}
 	}
 }
