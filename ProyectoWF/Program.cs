36d8dc65c7dda19efb595ae0e516125ab5ae679c
francisco lopez puerta <harcamelo@hotmail.com>
2017-11-17 20:10:21 +0100

    ultimos cambios con guillermo

--- ProyectoWF/Program.cs
+++ ProyectoWF/Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoWF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmListadoClientes());
+            Application.Run(new FrmListadoEmpleados());
         }
     }
 }
