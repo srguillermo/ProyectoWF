8388aa4c3e1833540d3e5718e251eb51d7aa1fcd
Cristian <kaxlolz@gmail.com>
2017-12-14 18:30:13 +0100

    sfdsdsf

--- ProyectoWF/FormularioProductos.cs
+++ ProyectoWF/FormularioProductos.cs
@@ -271,7 +271,9 @@ namespace ProyectoWF
             Conexion.cerrarConexion();
             Close();
         }
-
+        // Al pulsar sobre aceptar, dependiendo del modo en que estemos, haremos una cosa u otra.
+        // Si faltan datos obligatorios mostraremos un mensaje de advertencia.
+        // Modo 0(Alta): Añadimos 
         private void btAceptar_Click(object sender, EventArgs e)
         {
             if (modo == 0)
